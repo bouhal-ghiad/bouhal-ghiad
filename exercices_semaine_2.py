3b0199e073ec2c6dfdde77d5dbb46ee518fe39cf
bouhal-ghiad <100290733+bouhal-ghiad@users.noreply.github.com>
2022-03-01 11:49:11 -0500

    calcule de distance entre deux point

---
+++ exercices_semaine_2.py
@@ -0,0 +1,27 @@
+def riad(p1, p2, p3, p4):
+    return((p1+p2)*p3)/p4
+x=6
+y=4
+z=3 
+t=2
+
+print(riad(x,y,z,t))
+
+def riad2(p1, p2, p3, p4):
+    return((p1+p2+p3+p4)/4, p1+p2+p3+p4)
+
+n1=2
+n2=3
+n3=4
+n4=5
+
+print(riad2(n1,n2,n3,n4))
+
+def riad3(x, y, z):
+    return((x+y)/z)
+
+t1=2
+t2=3
+t3=4
+
+print(riad3(t1,t2,t3))
